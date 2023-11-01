722f6f0e35fb3e995f23da34e3c6b961d5e441ab
Daksh <akshd@outlook.com>
2023-11-01 16:03:39 +0530

    feat: build AA wallet

--- main.ts
+++ main.ts
@@ -6,3 +6,31 @@
 // https://docs.stackup.sh/docs/get-started-with-stackup
 //
 // Enter `npx ts-node main.ts` into your terminal to run.
+
+import { ethers } from "ethers";
+import { Presets, Client } from "userop";
+import { config } from "dotenv";
+
+config();
+
+const main = async () => {
+
+    const paymasterRpcUrl = process.env.PAYMASTER_URL as string;
+    const paymasterContext = { type: "payg" }
+
+    const paymaster = Presets.Middleware.verifyingPaymaster(
+        paymasterRpcUrl,
+        paymasterContext
+    )
+
+    const signer = new ethers.Wallet(process.env.SIGNING_KEY as string);
+    const rpcUrl = process.env.RPC_URL as string;
+
+    const builder = await Presets.Builder.Kernel.init(signer, rpcUrl, { paymasterMiddleware: paymaster });
+
+    const address = builder.getSender();
+
+    console.log(address);
+}
+
+main()
